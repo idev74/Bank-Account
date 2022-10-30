dad9e75e3711932e9cb00586b6c6ddbd82c769ee
Isabella Devassy <77900249+idev74@users.noreply.github.com>
2022-10-30 12:08:55 -0700

    some-complete-methods

--- BankAccount.py
+++ BankAccount.py
@@ -1,15 +1,21 @@
 class BankAccount:
-    def __init__(self, full_name, account_number, routing_number, balance):
+    def __init__(self, full_name, account_number, routing_number, balance = 0):
         self.full_name = full_name
         self.account_number = account_number
         self. routing_number = routing_number
         self.balance = balance
 
     def deposit(self, amount):
-        pass
+        self.balance += amount
+        print(f'Amount deposited: {amount}')
 
     def withdraw(self, amount):
-        pass
+        self.balance -= amount
+        if self.balance < amount:
+         print('Insufficient funds.')
+         self.balance -= 10
+        else:
+            print(f'Amount Withdrawn: ${amount}')
 
     def get_balance(self):
         pass
@@ -17,5 +23,5 @@ class BankAccount:
     def add_interest(self):
         pass
 
-    def print_receipt(full_name, account_number, routing_number, balance):
-        print f'{full_name} \nAccount No.: {account_number}\nRouting No.: {routing_number}\nBalance: ${balance}'
+    def print_receipt(self, full_name, account_number, routing_number, balance):
+        print(f'{full_name} \nAccount No.: {account_number}\nRouting No.: {routing_number}\nBalance: ${balance}')
